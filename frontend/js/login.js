133ca8b3d7f6648f4a3ac94b974fe7f8d7a3391a
VDhanush75 <dhanushdhanush76593@gmail.com>
2026-01-06 06:04:00 +0530

    Update login API endpoint to production URL

--- frontend/js/login.js
+++ frontend/js/login.js
@@ -12,7 +12,7 @@ document.getElementById("loginBtn").addEventListener("click", async function () 
   }
 
   try {
-    const response = await fetch("http://127.0.0.1:5000/login", {
+    const response = await fetch("https://expense-tracker-53vr.onrender.com/login", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ userId, password })
